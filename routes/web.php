858a91978ad69239761b3a430fdfa7b75713e7ff
Julio César <99225585+adlgkjs@users.noreply.github.com>
2025-03-13 23:30:37 -0600

    Termine de agregar template

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,21 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('inicio');
+});
+
+Route::get('/nosotros', function () {
+    return view('nosotros');
+});
+
+Route::get('/contacto', function () {
+    return view('contacto');
+});
+
+Route::get('/campañas', function () {
+    return view('campañas');
+});
+
+Route::get('/productos', function () {
+    return view('productos');
 });
